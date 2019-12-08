af31f053f41eb5f469b37b9d2810d10655d504e3
AkimovAbibulla <akimov15@icloud.com>
2019-12-08 12:42:22 +0300

    good

---
+++ NeuralNetwork/NeuronType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeuralNetwork
+{
+   public enum NeuronType
+    {
+        Input = 0,
+        Normal = 1,
+        Output = 2
+    }
+}
---
+++ NeuralNetwork/Topology.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeuralNetwork
+{
+   public class Topology
+    {
+        public int InputCount { get; }
+        public int OutputCount { get; }
+        public List<int> HiddenLayers { get; }
+
+        public Topology(int inputCount, int outputCount, params int[] layers)
+        {
+            InputCount = inputCount;
+            OutputCount = outputCount;
+            HiddenLayers = new List<int>();
+            HiddenLayers.AddRange(layers);
+        }
+    }
+}
